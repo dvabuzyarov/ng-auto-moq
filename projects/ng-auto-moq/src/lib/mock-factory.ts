21cef3546acbad07c0ef5af3a807aaa73c0306d4
dvabuzyarov <dvabuzyarov@gmail.com>
2019-08-27 12:07:12 +0300

    fix(moq.ts v3): fix issue with moq.ts v3

--- projects/ng-auto-moq/src/lib/mock-factory.ts
+++ projects/ng-auto-moq/src/lib/mock-factory.ts
@@ -4,7 +4,7 @@ import { IMockedObject, IParameter } from "./types";
 export type DefaultMockFactory = typeof mockFactory;
 
 export function mockFactory(parameter: IParameter): IMock<any & IMockedObject<any>> {
-    const mock = new Mock<any & IMockedObject<any>>(parameter.displayName);
+    const mock = new Mock<any & IMockedObject<any>>({name: parameter.displayName});
     mock.setup(instance => instance.__mock).returns(mock);
     return mock;
 }
